22bb0b89c9c74c8a2ab6a2626658136fd7e59ed8
Julian Vazquez <julianv483@gmail.com>
2022-05-03 10:20:33 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState,useEffect} from 'react';
+import Timer from './Components/Timer';
+
 
 function App() {
+const [timerDays,setTimerDays] = useState();
+const [timerHours,setTimerHours] = useState();
+const [timerMinutes,setTimerMinutes] = useState();
+const [timerSeconds,setTimerSeconds] = useState();
+
+let interval;
+
+const startTimer = () =>{
+  const startDate= new Date("May 5, 2022").getTime();
+
+  interval=setInterval(()=>{        /*Send back Date, Time */
+    const now = new Date().getTime();
+
+    const distance = startDate - now;
+
+    const days= Math.floor(distance/(24*60*60*1000));    /*floor for intiger*/ /*divide with a day*/
+    const hours= Math.floor((distance % (24*60*60*1000))/(1000*60*60));
+    const minutes= Math.floor((distance % (60*60*1000))/(1000*60));
+    const seconds= Math.floor((distance % (60*1000))/(1000));
+
+    
+    if (distance<0) {
+      //stop the timer
+
+      clearInterval(interval.current);
+    }
+    else {
+      //update Timer
+      setTimerDays(days);
+      setTimerHours(hours);
+      setTimerMinutes(minutes);
+      setTimerSeconds(seconds);
+
+    }
+  });
+};
+  useEffect(()=>{
+    startTimer();
+  });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Timer
+      timerDays={timerDays}
+      timerHourss={timerHours}
+      timerMinutes={timerMinutes}
+      timerSeconds={timerSeconds}
+      />
     </div>
   );
-}
+};
 
 export default App;
